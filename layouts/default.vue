c7ea008cf4c438ae97683ab8995857b5c6aa34f1
MostafaZayed72 <zayed8646@gmail.com>
2024-07-04 02:59:09 +0300

    test3

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,10 +2,10 @@
   <div style="direction: rtl;" class="dark:bg-dark1 bg-gray-300 dark:text-white">
     <v-layout>
       <v-navigation-drawer
-      v-if="$colorMode.preference === 'light'"
+     
         location="right"
         permanent
-        class="bg-blue-darken-1"
+        :class="$colorMode.preference === 'dark'? 'bg-blue-darken-3':'bg-blue-darken-1'"
         
       >
         <template v-slot:prepend>
@@ -29,34 +29,7 @@
           <v-list-item prepend-icon="mdi-gavel" title="جميع القضايا" value="users"></v-list-item>
         </v-list>
       </v-navigation-drawer>
-      <v-navigation-drawer
-      v-else
-        location="right"
-        permanent
-        class="bg-blue-darken-4"
-        
-      >
-        <template v-slot:prepend>
-          <div class="flex flex-col">
-            <div class="flex justify-center items-center gap-10">
-              <img class="w-[20%] " src="/assets/images/logo.png" alt="">
-              <color-mode /> <!-- استدعاء مكون اللون هنا -->
-            </div>
-            <v-list-item
-              lines="two"
-              prepend-avatar="https://media-hbe1-1.cdn.whatsapp.net/v/t61.24694-24/445156387_434453806052029_8839455247940985259_n.jpg?ccb=11-4&oh=01_Q5AaIG_mOtKGSomW6-qC0lK8ZpXSPLKmeBMkPTJkCYLhss9c&oe=6691058A&_nc_sid=e6ed6c&_nc_cat=107"
-              subtitle="Logged in"
-              title="أ/عبدالله"
-            ></v-list-item>
-          </div>
-        </template>
-
-        <v-list density="compact" nav>
-          <v-list-item prepend-icon="mdi-home-city" title="الرئيسية" value="home"></v-list-item>
-          <v-list-item prepend-icon="mdi-account" title="حسابي" value="account"></v-list-item>
-          <v-list-item prepend-icon="mdi-gavel" title="جميع القضايا" value="users"></v-list-item>
-        </v-list>
-      </v-navigation-drawer>
+      
 
       <v-main class="h-screen mr-4">
         <slot /> <!-- هنا يجب أن يكون الـ slot داخل الـ v-main -->
