d075354d5406496f49dcc0e0c1fd5afe9baefdad
Mostafa Zayed <143009621+MostafaZayed72@users.noreply.github.com>
2024-07-19 19:30:03 +0300

    sss

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,7 +9,7 @@
               <img class="w-[20%]" src="/assets/images/logo.png" alt="">
               <color-mode />
             </div>
-            <v-list-item lines="two" style="direction: rtl;"
+            <v-list-item lines="two" style="direction: rtl;" v-if="jwt"
               prepend-avatar="https://t4.ftcdn.net/jpg/02/29/75/83/360_F_229758328_7x8jwCwjtBMmC6rgFzLFhZoEpLobB6L8.jpg"
               subtitle="Logged in" :title=userName>
             </v-list-item>
